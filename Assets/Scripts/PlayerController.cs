73d430fe2910dc40ced6952c689537e1e3b406f3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-03-15 16:05:59 +0700

    Object Count and text update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
-	private Rigidbody rb;
 
-	void Start() {
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start() 
+	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
-	void FixedUpdate() {
+	void FixedUpdate() 
+	{
 
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -19,5 +30,24 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce(movement * speed);
 	
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();	
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 14) 
+		{
+			winText.text = "You win Malaka";
+		}
 	}
 }
